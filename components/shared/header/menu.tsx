94a98238f32a7d67e9d66678495f0f4e2ca4a5c7
Steve Johann <sjohann@protonmail.com>
2025-04-15 15:00:30 -0400

    14-signin-user

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,16 +1,12 @@
-
-import Link from 'next/link'
 import CartButton from './cart-button'
+import UserButton from './user-button'
 
 
 export default function Menu() {
     return (
         <div className='flex justify-end'>
             <nav className='flex gap-3 w-full'>
-                <Link href='/signin' className='flex items-center header-button'>
-                    {/* <UserIcon className='h-8 w-8' /> */}
-                    <span className='font-bold'>Sign in</span>
-                </Link>
+                <UserButton />
 
                 <CartButton />
             </nav>
--- types/index.ts
+++ types/index.ts
@@ -3,10 +3,13 @@ import {
   CartSchema,
   OrderItemSchema,
   ProductInputSchema,
+  UserInputSchema,
+  UserSignInSchema,
 } from '@/lib/validator'
 
 export type IProductInput = z.infer<typeof ProductInputSchema>
 export type Data = {
+  users: IUserInput[]
   products: IProductInput[]
   headerMenus: {
     name: string
@@ -23,3 +26,7 @@ export type Data = {
 
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
+
+// user
+export type IUserInput = z.infer<typeof UserInputSchema>
+export type IUserSignIn = z.infer<typeof UserSignInSchema>
